f1344418cb92641498308ffaa95dcf37a933d31a
Roger Steve Ruiz <hi@rog.gr>
2021-03-07 16:53:21 -0500

    Add in the script

---
+++ get_data_from_cvs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+areAnyAvailable="false"
+
+currentDirectory=$(dirname "${0}")
+
+towns=$(cat ${currentDirectory}/towns.txt)
+
+phoneNumber=$(cat ${currentDirectory}/phone-number.txt)
+api_key=$(cat ${currentDirectory}/textbelt-api-key.txt)
+linkToWebsite="https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-link2-coronavirus-vaccine#acc_link_content_section_box_251541438_boxpar_accordion_910919113_2"
+
+rm -rf ${currentDirectory}/cvs-output.json
+
+curl \
+  -s \
+  $(cat ${currentDirectory}/api-endpoint.txt) \
+  -H "$(cat ${currentDirectory}/header-user-agent.txt)" \
+  -H 'Accept: */*' \
+  -H 'Accept-Language: es-MX,es;q=0.8,en-US;q=0.5,en;q=0.3' \
+  --compressed \
+  -H  "$(cat ${currentDirectory}/header-referer.txt)"\
+  -H 'Connection: keep-alive' \
+  -H "$(cat ${currentDirectory}/cookie-data.txt)" \
+  -H 'TE: Trailers' > ${currentDirectory}/cvs-output.json
+
+for t in ${towns}
+do
+
+  areAnyAvailable=$(
+    jq --arg t ${t} \
+      -r \
+      '.responsePayloadData.data.NY[] | select(.city == $t) | {"status": .status} | .status == "Available"' \
+      -- ${currentDirectory}/cvs-output.json
+  )
+
+  if [[ "${areAnyAvailable}" == 'true' ]]
+  then
+    echo "Yes vaccines available at ${t} CVS! $(date)"
+    curl -X POST https://textbelt.com/text \
+      --data-urlencode phone=${phoneNumber} \
+      --data-urlencode message="There are vaccines available at ${t} CVS! $(date) ${linkToWebsite}" \
+      -d key=${api_key}
+  else
+    echo "No vaccines available at ${t} CVS. $(date)"
+  fi
+
+done
